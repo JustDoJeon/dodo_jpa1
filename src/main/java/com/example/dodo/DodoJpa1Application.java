66aa00ce49a5ef84b0a68d72928529fffab4fbc2
JeonDoHyun <52389219+JeonDoHyun@users.noreply.github.com>
2022-08-01 21:34:18 +0900

    Initial commit

---
+++ src/main/java/com/example/dodo/DodoJpa1Application.java
@@ -0,0 +1,13 @@
+package com.example.dodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DodoJpa1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DodoJpa1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dodo/DodoJpa1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DodoJpa1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
