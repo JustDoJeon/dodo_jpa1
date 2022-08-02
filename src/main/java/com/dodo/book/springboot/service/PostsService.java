986487a738016ffd9ff444ed169114bd4b88e17e
JeonDoHyun <52389219+JeonDoHyun@users.noreply.github.com>
2022-08-03 00:38:31 +0900

    Spring web Layer

---
+++ src/main/java/com/dodo/book/springboot/service/PostsService.java
@@ -0,0 +1,20 @@
+package com.dodo.book.springboot.service;
+
+import com.dodo.book.springboot.domain.posts.PostsRepository;
+import com.dodo.book.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+}
---
+++ src/main/java/com/dodo/book/springboot/web/dto/PostsSaveRequestDto.java
@@ -0,0 +1,28 @@
+package com.dodo.book.springboot.web.dto;
+
+import com.dodo.book.springboot.domain.posts.Posts;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class PostsSaveRequestDto {
+    private String title;
+    private String content;
+    private String author;
+
+    @Builder
+    public PostsSaveRequestDto(String title, String content,String author){
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+
+    public Posts toEntity() {
+        return Posts.builder()
+                .title(title)
+                .author(author)
+                .content(content).build();
+    }
+}
