0fd816731fbc0e52b440ef438e97b44fe8619304
JeonDoHyun <52389219+JeonDoHyun@users.noreply.github.com>
2022-09-23 00:49:30 +0900

    구글 로그인

--- src/main/java/com/dodo/book/springboot/web/IndexController.java
+++ src/main/java/com/dodo/book/springboot/web/IndexController.java
@@ -1,9 +1,11 @@
 package com.dodo.book.springboot.web;
 
+import com.dodo.book.springboot.config.auth.dto.SessionUser;
 import com.dodo.book.springboot.domain.posts.Posts;
 import com.dodo.book.springboot.service.PostsService;
 import com.dodo.book.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,15 +13,24 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model){
         model.addAttribute("posts",postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+        if(user != null){
+            model.addAttribute("userName",user.getName());
+        }
+
         return "index";
     }
 
